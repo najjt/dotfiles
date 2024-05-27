e30ed6c8d39eb08eea0103bab3fe769091da51cf
Martin Lönn Andersson <mlonna@pm.me>
2024-05-27 18:40:18 +0200

    made battery label indicate charging status

--- dot_config/sketchybar/plugins/executable_battery.sh
+++ dot_config/sketchybar/plugins/executable_battery.sh
@@ -2,10 +2,17 @@
 
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 TIME=$(pmset -g batt | grep -Eo "\d+:\d+" | cut -d% -f1)
-LABEL="${PERCENTAGE}% $TIME"
+CHARGING=$(pmset -g batt | grep -Eo "\w*charging" | cut -d% -f1)
+LABEL=""
 
 if [ $PERCENTAGE = "" ]; then
     exit 0
+fi
+
+if [ $CHARGING = "discharging" ]; then
+    LABEL="${PERCENTAGE}% $TIME"
+elif [ $CHARGING = "charging" ]; then
+    LABEL="${PERCENTAGE}% (charging)"
 fi
 
 sketchybar --set $NAME label="| BAT: $LABEL"
