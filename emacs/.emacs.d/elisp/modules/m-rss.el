6a7712feb9e30c52e44d3a330f4a7ec04be35970
Martin Lönn Andersson <mlonna@pm.me>
2024-11-23 18:26:16 +0000

    Emacs: re-order rss feeds, add comment and add new feed

--- emacs/.emacs.d/elisp/modules/m-rss.el
+++ emacs/.emacs.d/elisp/modules/m-rss.el
@@ -2,14 +2,18 @@
 
 (use-package elfeed
   :bind ("C-c w" . elfeed)
+  :hook (elfeed-search-mode . elfeed-update)
   :config
   (setq elfeed-feeds
-        '("https://protesilaos.com/commentary.xml"
+        '(;; Prot's commentary on life
+          "https://protesilaos.com/commentary.xml"
+          ;; Prot's youtube
+          "https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g"
           ;; Dax Flame
           "https://www.youtube.com/feeds/videos.xml?channel_id=UCG2JqxPd0Yd_XPK9shr-J_w"
           ;; Gavin Freeborn
           "https://www.youtube.com/feeds/videos.xml?channel_id=UCJetJ7nDNLlEzDLXv7KIo0w"
-          ;; Protesilaos Stavrou
-          "https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g")))
+          ;; Bread on Penguins
+          "https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw")))
 
 (provide 'm-rss)
