3f8e76882fae08bbe89a5063f026079b2686a90a
Martin Lönn Andersson <mlonna@pm.me>
2024-11-27 18:31:20 +0000

    Emacs: update comments

--- emacs/.emacs.d/elisp/modules/m-rss.el
+++ emacs/.emacs.d/elisp/modules/m-rss.el
@@ -9,13 +9,13 @@
   (setq elfeed-feeds
         '(;; Prot's commentary on life
           "https://protesilaos.com/commentary.xml"
-          ;; Prot's youtube
+          ;; Prot's youtube channel
           "https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g"
-          ;; Dax Flame
+          ;; Dax Flame's youtube channel
           "https://www.youtube.com/feeds/videos.xml?channel_id=UCG2JqxPd0Yd_XPK9shr-J_w"
-          ;; Gavin Freeborn
+          ;; Gavin Freeborn's youtube channel
           "https://www.youtube.com/feeds/videos.xml?channel_id=UCJetJ7nDNLlEzDLXv7KIo0w"
-          ;; Bread on Penguins
+          ;; Bread on Penguins's youtube channel
           "https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw")))
 
 (provide 'm-rss)
