ddadf6f783818e2d6b27b0116d16c7a7e5683a0b
Martin Lönn Andersson <mlonna@pm.me>
2024-12-08 19:38:13 +0000

    zsh: formatting

--- zsh/custom/linux.zsh
+++ zsh/custom/linux.zsh
@@ -1,4 +1,8 @@
-# Prompt theme
-source ~/powerlevel10k/powerlevel10k.zsh-theme
+# Systemd services
+alias ecre="systemctl --user restart emacs"     # Restart Emacs service
+alias stre="systemctl --user restart syncthing" # Restart Syncthing service
 
 alias dnf="sudo dnf"
+
+# Prompt theme
+source ~/programs/powerlevel10k/powerlevel10k.zsh-theme
