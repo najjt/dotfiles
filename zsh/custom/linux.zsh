5026aa72820fde3c51b917570979948818a29786
Martin Lönn Andersson <mlonna@pm.me>
2024-12-08 19:38:22 +0000

    zsh: add vpn aliases

--- zsh/custom/linux.zsh
+++ zsh/custom/linux.zsh
@@ -4,5 +4,8 @@ alias stre="systemctl --user restart syncthing" # Restart Syncthing service
 
 alias dnf="sudo dnf"
 
+alias vpn-up="wg-quick up sto11-adb"
+alias vpn-down="wg-quick down sto11-adb"
+
 # Prompt theme
 source ~/programs/powerlevel10k/powerlevel10k.zsh-theme
