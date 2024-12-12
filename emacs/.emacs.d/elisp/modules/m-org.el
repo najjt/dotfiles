c5da7d21f4a42747262a2f83e396ad1675a4d2be
Martin Lönn Andersson <mlonna@pm.me>
2024-12-12 21:54:24 +0000

    Emacs: add statistics view for org habits

--- emacs/.emacs.d/elisp/modules/m-org.el
+++ emacs/.emacs.d/elisp/modules/m-org.el
@@ -197,4 +197,11 @@ has no effect."
   :init
   (require 'ox-org))
 
+;; View statistics for habits
+(use-package org-habit-stats
+  :bind ((:map org-mode-map
+              ("C-c h" . org-habit-stats-view-habit-at-point))
+         (:map org-agenda-mode-map
+               ("C-c h" . org-habit-stats-view-habit-at-point-agenda))))
+
 (provide 'm-org)
