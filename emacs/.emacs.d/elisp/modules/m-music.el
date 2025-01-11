771b89df2ca600861e0faba58ae09406283c086f
Martin <najjt@mpad>
2025-01-11 18:36:58 +0000

    Emacs: hook hl-line-mode to emms modes

--- emacs/.emacs.d/elisp/modules/m-music.el
+++ emacs/.emacs.d/elisp/modules/m-music.el
@@ -5,6 +5,7 @@
          ("C-c e b" . emms-browser)
          :map emms-browser-mode-map
          ("C-a" . emms-browser-add-tracks-and-play))
+  :hook ((emms-browser-mode emms-playlist-mode) . hl-line-mode)
   :config
   (emms-all)
   (setq emms-player-list '(emms-player-mpv)
