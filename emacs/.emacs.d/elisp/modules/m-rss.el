79533eaaf17c491871f5ea152f2f635cc651cd0e
Martin <najjt@mpad>
2025-01-12 17:32:45 +0000

    Emacs: remove rss feeds

--- emacs/.emacs.d/elisp/modules/m-rss.el
+++ emacs/.emacs.d/elisp/modules/m-rss.el
@@ -11,10 +11,6 @@
           "https://protesilaos.com/commentary.xml"
           ;; Prot's youtube channel
           "https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g"
-          ;; Dax Flame's youtube channel
-          "https://www.youtube.com/feeds/videos.xml?channel_id=UCG2JqxPd0Yd_XPK9shr-J_w"
-          ;; Gavin Freeborn's youtube channel
-          "https://www.youtube.com/feeds/videos.xml?channel_id=UCJetJ7nDNLlEzDLXv7KIo0w"
           ;; Bread on Penguins's youtube channel
           "https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw")))
 
