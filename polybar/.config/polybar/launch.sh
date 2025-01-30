dd846b0cd6b242a76505ec1b13860f3e2fa8c209
Martin <najjt@localhost.localdomain>
2025-01-30 12:27:05 +0100

    polybar: change comment

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
+# Launch bar1
 echo "---" | tee -a /tmp/polybar1.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
