53ee281118540dac16c69fffadef614e41be6eca
Martin <najjt@localhost.localdomain>
2025-02-15 13:54:07 +0100

    Emacs: change dashboard items and appearance

--- emacs/.emacs.d/elisp/modules/m-dashboard.el
+++ emacs/.emacs.d/elisp/modules/m-dashboard.el
@@ -5,21 +5,20 @@
   (dashboard-setup-startup-hook)
   (setq initial-buffer-choice (lambda () (get-buffer-create dashboard-buffer-name))
         dashboard-center-content t
-        dashboard-vertically-center-content t
-        dashboard-items '((recents   . 5)
-                    (bookmarks . 5))
+        dashboard-items '((bookmarks . 5)
+                          (recents . 5))
+        dashboard-path-style 'truncate-middle
+        dashboard-path-max-length 25
         dashboard-startup-banner "~/.emacs.d/banner.txt"
         dashboard-startupify-list '(dashboard-insert-banner
                                     dashboard-insert-newline
                                     dashboard-insert-banner-title
-                                    dashboard-insert-newline
                                     dashboard-insert-navigator
                                     dashboard-insert-newline
                                     dashboard-insert-init-info
                                     dashboard-insert-items)
         dashboard-display-icons-p t
         dashboard-icon-type 'nerd-icons
-        dashboard-set-heading-icons nil
         dashboard-set-file-icons t))
 
 (provide 'm-dashboard)
