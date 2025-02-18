0c42b293953b929f0f67b05a8e4cf3d0a9c0d170
Martin <najjt@localhost.localdomain>
2025-02-18 21:09:14 +0100

    Emacs: show current song notification with elisp functions in EMMS

--- emacs/.emacs.d/elisp/modules/cfg-music.el
+++ emacs/.emacs.d/elisp/modules/cfg-music.el
@@ -15,12 +15,42 @@
         emms-playlist-mode-window-width 75
         emms-mode-line-format "")
 
-  ;; Function to send a notification to dunst with the song details and album art
-  (defun emms-show-notification ()
-    "Send a notification with the current track, artist, album name, and cover art."
-    (start-process-shell-command "notify-song" nil "$HOME/scripts/notify_song.sh"))
+  (defun emms-get-album-art (track-path)
+    "Get the album art for the track located at TRACK-PATH.
+Returns the path to the album art or a default image if no album art is found."
+    (let* ((album-dir (file-name-directory track-path))
+           (album-art (or (and (file-exists-p (concat album-dir "cover.jpg"))
+                               (concat album-dir "cover.jpg"))
+                          (and (file-exists-p (concat album-dir "folder.jpg"))
+                               (concat album-dir "folder.jpg"))
+                          (and (file-exists-p (concat album-dir "cover.png"))
+                               (concat album-dir "cover.png"))
+                          "~/.local/share/icons/Papirus-Dark/16x16/actions/media-play.svg")))
+      album-art))
 
-  ;; Hook into the track change event to trigger the notification
-  (add-hook 'emms-player-started-hook #'emms-show-notification))
+  (defun emms-notify-track-description ()
+    "Use `notify-send' to show the description of the current track with album art."
+    (interactive)
+    (let* ((track (emms-playlist-current-selected-track))
+           (title (emms-track-get track 'info-title))
+           (artist (emms-track-get track 'info-albumartist))
+           (album (emms-track-get track 'info-album))
+           (track-path (emms-track-get track 'name))
+           (album-art (emms-get-album-art track-path))
+           (default-album-art "~/.local/share/icons/Papirus-Dark/16x16/actions/media-play.svg"))
+
+      ;; Trim any extra whitespace from the results
+      (setq title (string-trim title))
+      (setq artist (string-trim artist))
+      (setq album (string-trim album))
+
+      ;; Send the notification
+      (call-process "notify-send"
+                    nil nil nil
+                    "-i" album-art
+                    title
+                    (format "%s\n<i>%s</i>" artist album))))
+
+  (add-hook 'emms-player-started-hook #'emms-notify-track-description))
 
 (provide 'cfg-music)
