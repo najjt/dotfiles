174d89b7d241f91208157146762bf88f8fc367f2
Martin <najjt@localhost.localdomain>
2025-02-19 09:41:31 +0100

    Emacs: change song notification format for EMMS

--- emacs/.emacs.d/elisp/modules/cfg-music.el
+++ emacs/.emacs.d/elisp/modules/cfg-music.el
@@ -52,8 +52,8 @@ Returns the path to the album art or a default image if no album art is found."
       (call-process "notify-send"
                     nil nil nil
                     "-i" album-art
-                    title
-                    (format "%s\n<i>%s</i>" artist album))))
+                    "Now playing:"
+                    (format "%s, %s\n<i>%s</i>" title artist album))))
 
   (add-hook 'emms-player-started-hook #'emms-notify-track-description))
 
