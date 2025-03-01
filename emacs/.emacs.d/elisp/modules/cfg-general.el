508d87eff19d9373ecfa60e7f461ace532c6f9f9
Martin <najjt@localhost.localdomain>
2025-03-01 15:56:26 +0100

    Emacs: change default browser

--- emacs/.emacs.d/elisp/modules/cfg-general.el
+++ emacs/.emacs.d/elisp/modules/cfg-general.el
@@ -53,7 +53,7 @@
 
 ;; Set default browser
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "librewolf")
+      browse-url-generic-program "firefox-esr")
 
 ;; Increase large file warning threshold
 (setq large-file-warning-threshold 100000000)
