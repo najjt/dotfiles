58444552d6bf7d92afe8092e2e0f7fcccbfc3cf6
Martin <najjt@localhost.localdomain>
2025-03-11 20:45:37 +0100

    Emacs: formatting

--- emacs/.emacs.d/elisp/modules/cfg-dev.el
+++ emacs/.emacs.d/elisp/modules/cfg-dev.el
@@ -30,9 +30,11 @@
   :after lsp-mode
   :hook ((lsp-mode). company-mode)
   :bind (:map company-active-map
+  :bind
+  ((:map company-active-map
          ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
+   (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common)))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
