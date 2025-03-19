c6db811614ac78ba69dcba13a0264e0dc9ed69d8
Martin <najjt@localhost.localdomain>
2025-03-19 17:11:13 +0100

    Emacs: remove spacious padding package

--- emacs/.emacs.d/elisp/modules/cfg-ui.el
+++ emacs/.emacs.d/elisp/modules/cfg-ui.el
@@ -171,10 +171,4 @@
   (interactive)
   (my/resize-window nil -5))
 
-;; Increase padding between windows
-(use-package spacious-padding
-  :config
-  (spacious-padding-mode +1)
-  (setq spacious-padding-subtle-mode-line t))
-
 (provide 'cfg-ui)
