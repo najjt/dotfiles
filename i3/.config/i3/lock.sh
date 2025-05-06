4b7194666a5aa6556e13c3bc7a3f8a511b3ba98e
Martin <najjt@localhost.localdomain>
2025-05-06 15:33:44 +0200

    i3: use xidlehook for auto suspend

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,6 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -e
-xset s off dpms 0 10 0
-i3lock -c 000000 --ignore-empty-password --show-failed-attempts --nofork
-xset s off -dpms
+# Only exported variables can be used within the timer's command.
+export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+
+# Run xidlehook
+xidlehook \
+  `# Don't lock when there's a fullscreen application` \
+  --not-when-fullscreen \
+  `# Don't lock when there's audio playing` \
+  --not-when-audio \
+  `# Dim the screen after 90 seconds, undim if user becomes active` \
+  --timer 90 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+  `# Lock after 10 more seconds` \
+  --timer 10 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness; i3lock' \
+    '' \
+  `# Finally, suspend 5 seconds after it locks` \
+  --timer 5 \
+    'systemctl suspend' \
+    ''
