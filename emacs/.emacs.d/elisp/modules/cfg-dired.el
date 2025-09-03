45d089eb02ea763d982aa6b83f494b07d13b4ea2
Martin <najjt@localhost.localdomain>
2025-09-03 21:27:57 +0200

    Emacs: remove dired-omit settings

--- emacs/.emacs.d/elisp/modules/cfg-dired.el
+++ emacs/.emacs.d/elisp/modules/cfg-dired.el
@@ -3,21 +3,12 @@
 (use-package dired
   :ensure nil
   :custom
-  ;; Hide message when omitting files
-  (dired-omit-verbose nil)
-
   ;; Show in long listing format,
   ;; show hidden files,
   ;; show sizes in human-readable format
   ;; sort directories first,
   ;; use natural sort for version numbers within text
   (dired-listing-switches "-lah --group-directories-first -v")
-
-  ;; What files to hide in dired-omit-mode
-  (dired-omit-files
-   (rx (or (seq bol (? ".") "#")         ; Autosave files
-           (seq bol "." (not (any "."))) ; Dotfiles
-           (seq "~" eol))))              ; Backup files
 
   ;; No infinite dired buffers!
   (dired-kill-when-opening-new-dired-buffer t))
