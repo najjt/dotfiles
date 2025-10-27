4b571152257bdb86744c600e171a65d402347f77
Martin <najjt@localhost.localdomain>
2025-10-27 17:24:14 +0100

    Emacs: move isearch settings, add which key

--- emacs/.emacs.d/elisp/modules/cfg-help.el
+++ emacs/.emacs.d/elisp/modules/cfg-help.el
@@ -72,12 +72,6 @@
   (global-set-key (kbd "C-h k") #'helpful-key)
   (global-set-key (kbd "C-h x") #'helpful-command))
 
-(defun isearch-occur ()
-  "Invoke `occur' from within isearch and switch to the occur window."
-  (interactive)
-  (let ((case-fold-search isearch-case-fold-search))
-    (occur (if isearch-regexp isearch-string (regexp-quote isearch-string)))))
-
-(define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
+(which-key-mode 1)
 
 (provide 'cfg-help)
