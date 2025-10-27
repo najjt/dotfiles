d9b8f1e28875e66b12822ffa6139b81269eab638
Martin <najjt@localhost.localdomain>
2025-10-27 17:24:03 +0100

    Emacs: isearch and occur settings

--- emacs/.emacs.d/elisp/modules/cfg-general.el
+++ emacs/.emacs.d/elisp/modules/cfg-general.el
@@ -254,4 +254,49 @@
 ;; desktop notifications
 (setq alert-default-style 'libnotify)
 
+(defun my/select-window (window &rest _)
+  "Select WINDOW for display-buffer-alist"
+  (select-window window))
+
+;; Settings for occur window
+(setq display-buffer-alist
+      '(((or . ((derived-mode . occur-mode)))
+         (display-buffer-reuse-mode-window display-buffer-at-bottom)
+         (body-function . my/select-window)
+         (dedicated . t)
+         (preserve-size . (t . t)))))
+
+(use-package isearch
+  :ensure nil
+  :defer t
+  :config
+  ;; Open occur from current isearch results
+  (defun my/occur-from-isearch ()
+    (interactive)
+    (let ((query (if isearch-regexp
+               isearch-string
+             (regexp-quote isearch-string))))
+      (isearch-update-ring isearch-string isearch-regexp)
+      (let (search-nonincremental-instead)
+        (ignore-errors (isearch-done t t)))
+      (occur query)))
+
+  ;; use selection to search
+  (defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
+    (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
+        (progn
+          (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
+          (deactivate-mark)
+          ad-do-it
+          (if (not forward)
+              (isearch-repeat-backward)
+            (goto-char (mark))
+            (isearch-repeat-forward)))
+      ad-do-it))
+  :bind
+  (:map isearch-mode-map
+        ("C-o" . my/occur-from-isearch)
+        ("C-d" . isearch-forward-symbol-at-point)
+        ("C-h" . isearch-query-replace)))
+
 (provide 'cfg-general)
