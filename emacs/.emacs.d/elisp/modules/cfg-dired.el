f325404490b32a1db1711783c6c347605ca42842
Martin <najjt@localhost.localdomain>
2025-11-06 11:56:11 +0100

    Emacs: hide details in dired & change listing switch

--- emacs/.emacs.d/elisp/modules/cfg-dired.el
+++ emacs/.emacs.d/elisp/modules/cfg-dired.el
@@ -2,13 +2,14 @@
 
 (use-package dired
   :ensure nil
+  :hook (dired-mode . dired-hide-details-mode)
   :custom
   ;; Show in long listing format,
   ;; show hidden files,
   ;; show sizes in human-readable format
   ;; sort directories first,
   ;; use natural sort for version numbers within text
-  (dired-listing-switches "-lah --group-directories-first -v")
+  (dired-listing-switches "-lAhv --group-directories-first")
 
   ;; No infinite dired buffers!
   (dired-kill-when-opening-new-dired-buffer t))
