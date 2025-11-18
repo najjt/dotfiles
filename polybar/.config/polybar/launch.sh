0dcaa662039f407e0e2b5d8003d1ecc0b1280557
Martin <najjt@t14-debian>
2025-11-18 18:22:01 +0100

    formatting

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
 
 # Launch bar1
 echo "---" | tee -a /tmp/polybar1.log
