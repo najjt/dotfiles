0bd4fe611a69ed7f04768577d9225a02693b997e
Martin Lönn Andersson <martin@malon.se>
2026-03-21 17:49:40 +0100

    Emacs: simplify performance settings

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,36 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
-;; During early initialization, I increase the gc-cons-threshold
-;; temporarily to speed up the start up process. However, a large
-;; gc-cons-threshold may cause freezing and stuttering during long-term
-;; interactive use, so here I set it to a more stable setting
-(defvar better-gc-cons-threshold 4294967269 ; 512mb
-  "The default value to use for `gc-cons-threshold'.
-
-  If you experience freezing, decrease this.  If you experience stuttering, increase this.")
-
-(add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold better-gc-cons-threshold)))
-
-;; Garbage collect when Emacs is out of focus and avoid garbage
-;; collection when using the minibuffer
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (if (boundp 'after-focus-change-function)
-                (add-function :after after-focus-change-function
-                              (lambda ()
-                                (unless (frame-focus-state)
-                                  (garbage-collect))))
-              (add-hook 'after-focus-change-function 'garbage-collect))
-            (defun gc-minibuffer-setup-hook ()
-              (setq gc-cons-threshold (* better-gc-cons-threshold 2)))
-
-            (defun gc-minibuffer-exit-hook ()
-              (garbage-collect)
-              (setq gc-cons-threshold better-gc-cons-threshold))
-
-            (add-hook 'minibuffer-setup-hook #'gc-minibuffer-setup-hook)
-            (add-hook 'minibuffer-exit-hook #'gc-minibuffer-exit-hook)))
+;; Performance
+(setq gc-cons-threshold (* 50 1000 1000))
+(setq read-process-output-max (* 1024 1024))
 
 ;; Make customize-based setting live in the custom.el file
 (setq custom-file "~/.config/emacs/custom.el")
