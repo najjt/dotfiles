588bd44934c590b5ce9981833795d13a2ae4aa16
Martin Lönn Andersson <martin@malon.se>
2026-03-30 12:52:06 +0200

    Emacs: move dired settings

--- emacs/.config/emacs/elisp/modules/cfg-general.el
+++ emacs/.config/emacs/elisp/modules/cfg-general.el
@@ -168,4 +168,19 @@
   :config
   (setq multi-vterm-dedicated-window-height-percent 30))
 
+(use-package dired
+  :ensure nil
+  :hook (dired-mode . dired-hide-details-mode)
+  :custom
+  ;; Show in long listing format,
+  ;; show hidden files,
+  ;; show sizes in human-readable format
+  ;; sort directories first,
+  ;; use natural sort for version numbers within text
+  (dired-listing-switches "-lAhv --group-directories-first")
+
+  ;; No infinite dired buffers!
+  (dired-kill-when-opening-new-dired-buffer t))
+
+(use-package dired-open)
 (provide 'cfg-general)
