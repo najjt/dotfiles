8722e35b6686204f121592e8bc7a1c91f9165e3a
Martin Lönn Andersson <martin@malon.se>
2026-04-17 19:30:44 +0200

    Emacs: formatting

--- emacs/.config/emacs/elisp/modules/cfg-windows.el
+++ emacs/.config/emacs/elisp/modules/cfg-windows.el
@@ -9,14 +9,7 @@
 
 ;; Make switching buffers more consistent
 (setopt switch-to-buffer-obey-display-actions t
-        switch-to-buffer-in-dedicated-window 'pop)
-
-;; Manage windows
-(use-package ace-window
-  :bind ("M-o" . ace-window)
-  :custom (aw-scope 'frame)
-  :config
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+	switch-to-buffer-in-dedicated-window 'pop)
 
 ;; Automatically switch focus to new window when it is created
 (defun my/split-and-follow-horizontally ()
@@ -38,26 +31,26 @@
   (interactive)
   (if (= (count-windows) 2)
       (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
 (global-set-key (kbd "C-x /") 'toggle-window-split)
 
 ;; Don't open a new window for ediff
@@ -69,6 +62,13 @@
 ;; Auto focus help-mode windows
 (setq help-window-select t)
 
+;; Manage windows
+(use-package ace-window
+  :bind ("M-o" . ace-window)
+  :custom (aw-scope 'frame)
+  :config
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+
 ;; Popup windows
 (use-package popper
   :defer nil
@@ -79,25 +79,25 @@
   ("C-c `"   . popper-toggle)
   :init
   (setq popper-reference-buffers
-        '("\\*Compile-Log\\*"
-          "^\\*compilation.*\\*$"
-          "Output\\*$"
-          "\\*Async Shell Command\\*"
-          "^\\*tex-shell.*\\*$"
-          "^\\*Flycheck.*\\*$"
-          "^\\*LSP Error List*\\*$"
-          "^\\*vterminal - dedicated\\*"
-          magit-mode
-          comint-mode
-          shell-mode
-          term-mode
-          help-mode
-          helpful-mode
-          compilation-mode
-          mpdel-song-mode))
+	'("\\*Compile-Log\\*"
+	  "^\\*compilation.*\\*$"
+	  "Output\\*$"
+	  "\\*Async Shell Command\\*"
+	  "^\\*tex-shell.*\\*$"
+	  "^\\*Flycheck.*\\*$"
+	  "^\\*LSP Error List*\\*$"
+	  "^\\*vterminal - dedicated\\*"
+	  magit-mode
+	  comint-mode
+	  shell-mode
+	  term-mode
+	  help-mode
+	  helpful-mode
+	  compilation-mode
+	  mpdel-song-mode))
   :config
   (setq popper-mode-line " POP " ; Let it breathe a bit
-        popper-window-height 19)
+	popper-window-height 19)
   (popper-mode 1)
   (popper-echo-mode 1))
 
