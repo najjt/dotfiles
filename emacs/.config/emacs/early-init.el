b14543256d4dc02058a2c57224aa707f139a7456
Martin <martin@malon.se>
2026-05-12 18:28:18 +0200

    Emacs: remove timestamps from messages buffer

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -28,22 +28,3 @@
       byte-compile-warnings '(not obsolete)
       warning-suppress-log-types '((comp) (bytecomp))
       native-comp-async-report-warnings-errors 'silent)
-
-;; Show timestamps in *Messages* buffer
-;; Source: https://emacs.stackexchange.com/a/38511
-(defun my/ad-timestamp-message (FORMAT-STRING &rest args)
-  "Advice to run before `message' that prepends a timestamp to each message.
-	Activate this advice with:
-	  (advice-add 'message :before 'my/ad-timestamp-message)
-	Deactivate this advice with:
-	  (advice-remove 'message 'my/ad-timestamp-message)"
-  (if message-log-max
-      (let ((deactivate-mark nil)
-	    (inhibit-read-only t))
-	(with-current-buffer "*Messages*"
-	  (goto-char (point-max))
-	  (if (not (bolp))
-	      (newline))
-	  (insert (format-time-string "[%F %T.%3N] "))))))
-
-(advice-add 'message :before 'my/ad-timestamp-message)
